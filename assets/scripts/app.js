ce05ad8c178ff8399952c92237a48a2709ba4f2e
Scott Harrington <scott@kingtidecreative.com>
2019-10-12 14:16:37 -0700

    Doing work

---
+++ assets/scripts/app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  var selectors = {
+  }
+
+  var classes = {
+  }
+
+  submitForm();
+
+})
+
+function submitForm() {
+  console.log('hi');
+}
